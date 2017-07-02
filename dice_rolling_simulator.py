5053a5ade48df55a3d945ae0e5f95838f0f3b492
rafaelvicari <rafaelgvicari@gmail.com>
2017-07-02 10:47:49 -0300

    Update dice_rolling_simulator.py

--- dice_rolling_simulator.py
+++ dice_rolling_simulator.py
@@ -6,26 +6,17 @@
 
 import random
 
-global user_exit_checker
-user_exit_checker="exit"
-
 def start():
-    print "Welcome to dice rolling simulator: \nPress Enter to proceed"
-    raw_input(">")
-
+    print "\n\n\nWelcome to dice rolling simulator: \n\nPress any key to proceed\n"
+    raw_input("> ")
     result()
 
 def bye():
-    print "Thanks for using the Dice Rolling Simulator! Have a great day! =)"
+    print "\nThanks for using the Dice Rolling Simulator! Have a great day!\n"
 
 def result():
-
-    #user_dice_chooser  No idea how this got in here, thanks EroMonsterSanji.
-
-
-    print "\r\nGreat! Begin by choosing a die! [6] [8] [12]?\r\n" 
-    user_dice_chooser = raw_input(">")
-
+    print "\nGreat! Begin by choosing a die! [6] [8] [12]?\n"
+    user_dice_chooser = raw_input("> ")
     user_dice_chooser = int(user_dice_chooser)
 
     if user_dice_chooser == 6:
@@ -38,40 +29,30 @@ def result():
         dice12()
 
     else:
-        print "\r\nPlease choose one of the applicable options!\r\n"
+        print "\nPlease choose one of the applicable options!\n"
         result()
 
+def user_exit_checker():
+    user_exit_checker_raw = raw_input("\nIf you want to roll another die, type [roll]. To exit, type [exit].\n\n> ")
+    user_exit_checker = (user_exit_checker_raw.lower())
+    if user_exit_checker == "roll":
+        result()
+    else:
+        bye()
 
 def dice6():
-    dice_6 = random.randint(1,6)
-    print "\r\nYou rolled a " + str(dice_6) + "!\r\n"
-
-    user_exit_checker_raw = raw_input("\r\nIf you want to roll another die, type [roll]. To exit, type [exit].\r\n?>")
-    user_exit_checker = (user_exit_checker_raw.lower())
-    if user_exit_checker=="roll":
-        start()
-    else:
-        bye()
+    dice_6 = random.randint(1, 6)
+    print "\r\nYou rolled a " + str(dice_6) + "!"
+    user_exit_checker()
 
 def dice8():
-    dice_8 = random.randint(1,8)
+    dice_8 = random.randint(1, 8)
     print "\r\nYou rolled a " + str(dice_8) + "!"
-
-    user_exit_checker_raw = raw_input("\r\nIf you want to roll another die, type [roll]. To exit, type [exit].\r\n?>")
-    user_exit_checker = (user_exit_checker_raw.lower())
-    if user_exit_checker=="roll":
-        start()
-    else:
-        bye()
+    user_exit_checker()
 
 def dice12():
-    dice_12 = random.randint(1,12)
+    dice_12 = random.randint(1, 12)
     print "\r\nYou rolled a " + str(dice_12) + "!"
+    user_exit_checker()
 
-    user_exit_checker_raw = raw_input("\r\nIf you want to roll another die, type [roll]. To exit, type [exit].\r\n?>")
-    user_exit_checker = (user_exit_checker_raw.lower())
-    if user_exit_checker=="roll":
-        start()
-    else:
-        bye()
 start()
